56e7cbd908ac277a44942d0357c2c1a3848188cd
m21kosumi <super19hpss@yahoo.co.jp>
2017-05-13 23:22:45 +0900

    no message

--- src/MyInterpolations.jl
+++ src/MyInterpolations.jl
@@ -1,2 +1,11 @@
 module MyInterpolations
+function my_lin_interp(grid, vals)
+    function func(x)
+        i = searchsortedfirst(grid, x)
+        x in grid[i-1]:grid[i]
+        return vals[i-1] + (vals[i]-vals[i-1])*(x - grid[i-1])/(grid[i] - grid[i-1])
+    end
+    return func
+end
+export my_lin_interp
 end
