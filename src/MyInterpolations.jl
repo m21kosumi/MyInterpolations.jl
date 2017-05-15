3b67769a5c7370dce1fa261b82e45bd427e2764d
m21kosumi <super19hpss@yahoo.co.jp>
2017-05-15 10:33:19 +0900

    no message

--- src/MyInterpolations.jl
+++ src/MyInterpolations.jl
@@ -10,10 +10,7 @@ function my_lin_interp(grid, vals)
         if x == grid[1]
             return vals[1]
         end
-        if x == grid[end]
-            return vals[end]
-        end
-        if grid[1] < x < grid[end]
+        if grid[1] < x <= grid[end]
         i = searchsortedfirst(grid, x)
         return vals[i-1] + (vals[i]-vals[i-1])*(x - grid[i-1])/(grid[i] - grid[i-1])
         end
